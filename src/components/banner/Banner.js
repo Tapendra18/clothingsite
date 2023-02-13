ab31958fe2defd5967a5db302a69f9dd463ec39e
Tapendra18 <107476177+Tapendra18@users.noreply.github.com>
2023-02-13 14:37:41 +0530

    fea banner api implement done

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,48 +1,40 @@
 
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Banner() {
+
+    const [data, setData] = useState();
+    const API_URL = "http://127.0.0.1:8000/";
+
+    const getusers = () => {
+        axios.get("http://127.0.0.1:8000/api/v1/feabanner")
+            .then(response => setData(response))
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        getusers();
+    }, []);
 
     return (
         <section className="banners mb-25">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-4 col-md-6">
-                        <div className="banner-img wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
-                            <img src="media/ads/middlebar/banner-3.png" alt="" />
-                            <div className="banner-text">
-                                <h4>
-                                    The best Offers Are Here
-                                </h4>
-                                <a href="product-details.html" className="btn btn-xs">Shop Now <i
-                                    className="fi-rs-arrow-small-right"></i></a>
+                    {data && data.data.data.map((item) => (
+                        <div className="col-lg-4 col-md-6" >
+                            <div className="banner-img wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
+                                <img style={{width:"500px" , height:"400px"}} src={`${API_URL + item.image}`} alt="" />
+                                <div className="banner-text">
+                                    <h4 style={{marginTop:"200px"}}>
+                                       {item.title}
+                                    </h4>
+                                    <a href="product-details.html" className="btn btn-xs">Shop Now <i
+                                        className="fi-rs-arrow-small-right"></i></a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                        <div className="banner-img wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
-                            <img src="media/ads/middlebar/banner-1.png" alt="" />
-                            <div className="banner-text">
-                                <h4>
-                                    New Cloth for all
-                                </h4>
-                                <a href="product-details.html" className="btn btn-xs">Shop Now <i
-                                    className="fi-rs-arrow-small-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                        <div className="banner-img wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
-                            <img src="media/ads/middlebar/banner-2.png" alt="" />
-                            <div className="banner-text">
-                                <h4>
-                                    Make your day <br /> stylist
-                                </h4>
-                                <a href="product-details.html" className="btn btn-xs">Shop Now <i
-                                    className="fi-rs-arrow-small-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
